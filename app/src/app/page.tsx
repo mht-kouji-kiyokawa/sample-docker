870c5f1918b434dae3398f8e89c8e256b30b98d1
mht-kouji-kiyokawa <k.kiyokawa@mht-code.com>
2024-03-02 23:17:32 +0900

    2つ目のdata.tsを追加

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -14,10 +14,11 @@ interface DataItem {
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 const Home: NextPage = () => {
-  const { data, error } = useSWR<DataItem[]>('/api/data', fetcher);
+  const { data: oldData, error: oldError } = useSWR<DataItem[]>('/api/data', fetcher);
+  const { data: newData, error: newError } = useSWR<DataItem[]>('/api/seconddata', fetcher); // 新しいAPIルート
 
-  if (error) return <div>データの取得に失敗しました。</div>;
-  if (!data) return <div>読み込み中...</div>;
+  if (oldError || newError) return <div>データの取得に失敗しました。</div>;
+  if (!oldData || !newData) return <div>読み込み中...</div>;
 
   return (
     <main className={styles.main}>
@@ -45,9 +46,15 @@ const Home: NextPage = () => {
         </div>
       </div>
       <div>
-        <h1>データ一覧</h1>
+        <h1>mytableのデータ一覧</h1>
           <ul>
-            {data.map((item) => (
+            {oldData.map((item: DataItem) => (
+              <li key={item.id}>{item.name}</li>
+            ))}
+          </ul>
+        <h1>mytable2のデータ一覧</h1>
+          <ul>
+            {newData.map((item: DataItem) => (
               <li key={item.id}>{item.name}</li>
             ))}
           </ul>
@@ -119,97 +126,3 @@ const Home: NextPage = () => {
 };
 
 export default Home;
-
-
-// export default function Home() {
-//   return (
-//     <main className={styles.main}>
-//       <div className={styles.description}>
-//         <p>
-//           Get started by editing&nbsp;
-//           <code className={styles.code}>src/app/page.tsx</code>
-//         </p>
-//         <div>
-//           <a
-//             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-//             target="_blank"
-//             rel="noopener noreferrer"
-//           >
-//             By{" "}
-//             <Image
-//               src="/vercel.svg"
-//               alt="Vercel Logo"
-//               className={styles.vercelLogo}
-//               width={100}
-//               height={24}
-//               priority
-//             />
-//           </a>
-//         </div>
-//       </div>
-
-//       <div className={styles.center}>
-//         <Image
-//           className={styles.logo}
-//           src="/next.svg"
-//           alt="Next.js Logo"
-//           width={180}
-//           height={37}
-//           priority
-//         />
-//       </div>
-
-//       <div className={styles.grid}>
-//         <a
-//           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-//           className={styles.card}
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           <h2>
-//             Docs <span>-&gt;</span>
-//           </h2>
-//           <p>Find in-depth information about Next.js features and API.</p>
-//         </a>
-
-//         <a
-//           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-//           className={styles.card}
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           <h2>
-//             Learn <span>-&gt;</span>
-//           </h2>
-//           <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-//         </a>
-
-//         <a
-//           href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-//           className={styles.card}
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           <h2>
-//             Templates <span>-&gt;</span>
-//           </h2>
-//           <p>Explore starter templates for Next.js.</p>
-//         </a>
-
-//         <a
-//           href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-//           className={styles.card}
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           <h2>
-//             Deploy <span>-&gt;</span>
-//           </h2>
-//           <p>
-//             Instantly deploy your Next.js site to a shareable URL with Vercel.
-//           </p>
-//         </a>
-//       </div>
-//     </main>
-//   );
-// }
